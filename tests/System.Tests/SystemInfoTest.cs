e9738acf1fa161079c8bc2e2594faf09a776b021
Stratos <StratosBlue@outlook.com>
2021-05-17 23:41:46 +0800

    增加系统信息获取静态类SystemInfo；

---
+++ tests/System.Tests/SystemInfoTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace System.Tests
+{
+    [TestClass]
+    public class SystemInfoTest
+    {
+        #region Public 方法
+
+        [TestMethod]
+        public void GenericUsage()
+        {
+            SystemInfo.Display();
+        }
+
+        #endregion Public 方法
+    }
+}
