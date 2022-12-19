0907bde1025b0b6900cd97d689c4c0ab00f88b18
Stratos <StratosBlue@outlook.com>
2022-12-19 19:49:10 +0800

    调整TFM；添加System.Threading.Task.Benchmark

---
+++ benchmarks/System.Threading.Task.Benchmark/TaskGeneralBenchmark.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace Benchmark
+{
+    [SimpleJob(RuntimeMoniker.Net60)]
+    [MemoryDiagnoser]
+    public class TaskGeneralBenchmark
+    {
+    }
+}
