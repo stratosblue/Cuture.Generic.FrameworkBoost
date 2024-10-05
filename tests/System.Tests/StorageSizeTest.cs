485a600e004504a8ed06a92c9a9619ca5aa2f653
stratos <stratosblue@outlook.com>
2024-10-05 14:07:59 +0800

    chore: 调整StorageSize

--- tests/System.Tests/StorageSizeTest.cs
+++ tests/System.Tests/StorageSizeTest.cs
@@ -57,5 +57,73 @@ public class StorageSizeTest
         Assert.AreEqual(b.GetHashCode(), c.GetHashCode());
     }
 
+    [TestMethod]
+    public void Should_Success_With_MaxValue()
+    {
+        var size = new StorageSize(long.MaxValue);
+        Assert.AreEqual("8EB", size.ToString());
+
+        size = new StorageSize(ulong.MaxValue);
+        Assert.AreEqual("16EB", size.ToString());
+    }
+
+    [TestMethod]
+    public void Should_Throw_With_ErrorValue()
+    {
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => new StorageSize(-1));
+        Assert.ThrowsException<ArgumentOutOfRangeException>(() => new StorageSize(long.MinValue));
+    }
+
+    [TestMethod]
+    public void Should_ToString_Correct()
+    {
+        const long B = 1;
+        const long KB = B * 1024;
+        const long MB = KB * 1024;
+        const long GB = MB * 1024;
+        const long TB = GB * 1024;
+        const long PB = TB * 1024;
+        const long EB = PB * 1024;
+
+        Assert.AreEqual("0B", new StorageSize(0).ToString());
+        Assert.AreEqual("1B", new StorageSize(B).ToString());
+        Assert.AreEqual("1023B", new StorageSize(KB - B).ToString());
+
+        Assert.AreEqual("1KB", new StorageSize(KB).ToString());
+        Assert.AreEqual("1.5KB", new StorageSize(KB + KB / 2).ToString());
+        Assert.AreEqual("1.499KB", new StorageSize(KB + KB / 2 - B).ToString());
+        Assert.AreEqual("1.999KB", new StorageSize(KB + KB - B).ToString());
+        Assert.AreEqual("1023KB", new StorageSize(MB - KB).ToString());
+
+        Assert.AreEqual("1MB", new StorageSize(MB).ToString());
+        Assert.AreEqual("1.5MB", new StorageSize(MB + MB / 2).ToString());
+        Assert.AreEqual("2MB", new StorageSize(MB + MB).ToString());
+        Assert.AreEqual("1.999MB", new StorageSize(MB + MB - KB).ToString());
+        Assert.AreEqual("1023MB", new StorageSize(GB - MB).ToString());
+
+        Assert.AreEqual("1GB", new StorageSize(GB).ToString());
+        Assert.AreEqual("1.5GB", new StorageSize(GB + GB / 2).ToString());
+        Assert.AreEqual("2GB", new StorageSize(GB + GB).ToString());
+        Assert.AreEqual("1.999GB", new StorageSize(GB + GB - MB).ToString());
+        Assert.AreEqual("1023GB", new StorageSize(TB - GB).ToString());
+
+        Assert.AreEqual("1TB", new StorageSize(TB).ToString());
+        Assert.AreEqual("1.5TB", new StorageSize(TB + TB / 2).ToString());
+        Assert.AreEqual("2TB", new StorageSize(TB + TB).ToString());
+        Assert.AreEqual("1.999TB", new StorageSize(TB + TB - GB).ToString());
+        Assert.AreEqual("1023TB", new StorageSize(PB - TB).ToString());
+
+        Assert.AreEqual("1PB", new StorageSize(PB).ToString());
+        Assert.AreEqual("1.5PB", new StorageSize(PB + PB / 2).ToString());
+        Assert.AreEqual("2PB", new StorageSize(PB + PB).ToString());
+        Assert.AreEqual("1.999PB", new StorageSize(PB + PB - TB).ToString());
+        Assert.AreEqual("1023PB", new StorageSize(EB - PB).ToString());
+
+        Assert.AreEqual("1EB", new StorageSize(EB).ToString());
+        Assert.AreEqual("1.5EB", new StorageSize(EB + EB / 2).ToString());
+        Assert.AreEqual("2EB", new StorageSize(EB + EB).ToString());
+        Assert.AreEqual("1.999EB", new StorageSize(EB + EB - PB).ToString());
+    }
+
     #endregion Public 方法
 }
